0ed7263d4b10a7f68ff462b344f206e61c653d05
adityaparsad01 <adityaparsad01@gmail.com>
2020-06-16 13:23:49 -0700

    pincode update

--- Pincode.jsx
+++ Pincode.jsx
@@ -1,2 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
+const Pincode = () =>{
+  const [num, setNum] = useState()
+  const [state, setState] = useState()
+  const [place, setPlace] = useState()
+
+
+
+  useEffect(()=>{
+    async function getData(){
+      const res = await axios.get(`https://api.zippopotam.us/IN/${num}`)
+      console.log(res.data.places.length)
+      setState(res.data.country )
+      setPlace(res.data.places.length)
+    }
+    getData()
+  })
+  return (<>
+  <h1>You PinCode is {num}  </h1>
+  <input type="number" value={num} onChange={(e)=>{
+    setNum(e.target.value)
+  }}>
+  </input>
+  <h1>{setState}</h1>
+  <h2>Total Number of Places in {num} is {place}</h2>
+  </>)
+} 
+export default Pincode
